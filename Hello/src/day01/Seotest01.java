cf2a4f8ffc1db05b042a88f6c356181957e7eb64
USER <USER@192.168.0.10>
2020-02-14 17:18:07 +0900

    day05 commit

---
+++ Hello/src/day01/Seotest01.java
@@ -0,0 +1,23 @@
+package day01;
+
+/**
+ *  이 클래스는 이클립스로 처음 제작한 클래스..
+ *  
+ * @author 연습
+ * @since	2020.02.11
+ * @version v.2.0
+ * @see
+ * 			코드 변경 이력
+ * 			2020.02.11 - 클래스 제작 - 담당자 : 서동혁
+ */
+public class Seotest01 {
+	
+	public static void main(String[] args) {
+		System.out.println("ddd");
+		// 단일행 주석
+		/*
+		 * 다중행 주석
+		 */
+		
+		}
+}
